500535d5bc4dbc2c354d632554b409a07d6cc740
Timothy B Aine <timothy.b.aine@gmail.com>
2022-07-25 19:00:26 +0300

    Update Conflict_data_Africa.py
    Signed-off-by: Timothy B Aine <timothy.b.aine@gmail.com>

--- Conflict_data_Africa.py
+++ Conflict_data_Africa.py
@@ -2,7 +2,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 import streamlit as st
-import plotly.express as px
+# import plotly.express as px
 import klib
 import pydeck as pdk
 import pandas as pd
